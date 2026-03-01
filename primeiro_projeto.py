a19fe536d173ef33b7596d42c8934838c9e7bc27
afonso-dev218 <araujoafonso074@gmail.com>
2026-03-01 15:16:50 -0300

    add primeiro projeto python
    funçao de saudaçao simples em python

---
+++ primeiro_projeto.py
@@ -0,0 +1,9 @@
+# primeiro projeto de python afonso
+# mostrar habilidades iniciais em programaçao
+
+def saudacao(nome):
+  return f"ola, {nome} bem vindo ao meu primeiro projeto python."
+
+# testando a funçao
+meu_nome = "afonso"
+print(saudacao(meu_nome))
